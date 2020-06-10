c4870f99b7a005e940f2e93360aae72180098602
lvleo21 <verasleonardo210@gmail.com>
2020-06-10 19:43:22 -0300

    remove sqlite

--- core/models.py
+++ core/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from atracoes.models import Atracao
 
 # Create your models here.
 class PontoTuristico(models.Model):
     nome = models.CharField(max_length=255)
     descricao = models.TextField()
     aprovado = models.BooleanField(default=False)
-    
+    atracoes = models.ManyToManyField(Atracao)
 
     def __str__(self):
         return self.nome
