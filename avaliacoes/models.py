17a4bdab8003faad76b698401a4bc5621fe3b23a
lvleo21 <verasleonardo210@gmail.com>
2020-06-10 20:11:00 -0300

    add models avaliacoes

--- avaliacoes/models.py
+++ avaliacoes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Avaliacao(models.Model):
+    usuario = models.ForeignKey(User, on_delete = models.CASCADE)
+    comentario = models.TextField(null =True, blank= True)
+    nota = models.DecimalField(max_digits=3, decimal_places=2)
+    data = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.usuario.username.upper()
